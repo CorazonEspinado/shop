32a0cd4343a1e98c49050f7120e6dcd8e2a7f49b
Arguss <roman.vyshedko@inbox.lv>
2019-01-08 21:40:09 +0200

    start generator

---
+++ database/migrations/2019_01_07_200237_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');//User making transaction
+            $table->integer('qrcode_id');
+            $table->string('payment_method')->nullable();
+            $table->integer('qrcode_owner_id')->nullable();
+            $table->string('message')->default('initiated');
+            $table->float('amount', 10, 4);
+            $table->string('status');//initiated, payment failed,completed and successull
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
