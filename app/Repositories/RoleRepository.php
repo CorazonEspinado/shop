0ec97c8db694db649bd111b142d41a2f52875478
Arguss <roman.vyshedko@inbox.lv>
2019-01-08 22:23:29 +0200

    generator created

---
+++ app/Repositories/RoleRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Role;
+use InfyOm\Generator\Common\BaseRepository;
+
+/**
+ * Class RoleRepository
+ * @package App\Repositories
+ * @version January 8, 2019, 8:13 pm UTC
+ *
+ * @method Role findWithoutFail($id, $columns = ['*'])
+ * @method Role find($id, $columns = ['*'])
+ * @method Role first($columns = ['*'])
+*/
+class RoleRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name'
+    ];
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Role::class;
+    }
+}
---
+++ app/Repositories/TransactionRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Transaction;
+use InfyOm\Generator\Common\BaseRepository;
+
+/**
+ * Class TransactionRepository
+ * @package App\Repositories
+ * @version January 8, 2019, 8:15 pm UTC
+ *
+ * @method Transaction findWithoutFail($id, $columns = ['*'])
+ * @method Transaction find($id, $columns = ['*'])
+ * @method Transaction first($columns = ['*'])
+*/
+class TransactionRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'user_id',
+        'qrcode_id',
+        'payment_method',
+        'qrcode_owner_id',
+        'message',
+        'amount',
+        'status'
+    ];
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Transaction::class;
+    }
+}
